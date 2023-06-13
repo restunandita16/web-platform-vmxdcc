24be519bdcb4f9a5f5c9fd324ab7750f16f43bc6
restunandita16 <136143487+restunandita16@users.noreply.github.com>
2023-06-13 18:01:33 +0700

    3 files modified

--- script.js
+++ script.js
@@ -16,3 +16,26 @@ buttons.forEach(button => {
     delete activeSlide.dataset.active
   })
 })
+
+/*Customer Review Section*/
+var swiper = new Swiper('.review-slider', {
+  loop:true,
+  spaceBetween: 10,
+  grabCursor: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  centeredSlides: true,
+  breakpoints: {
+    0: {
+      slidesPerView:1,
+    },
+    768: {
+      slidesPerView:2,
+    },
+    1020: {
+      slidesPerView: 3,
+    },
+  },
+});
